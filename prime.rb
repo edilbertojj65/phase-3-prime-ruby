54c3c9c147ffa776f482a1abe4deef6ef7f553c2
edilbertojj65 <edilbertojj65@gmail.com>
2021-07-06 15:58:33 -0400

    Completed

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+# A prime number is a numeral that is greater than 1 and cannot be divided evenly by any other number except 1 and itself. 
+# If a number can be divided evenly by any other number not counting itself and 1, 
+# it is not prime and is referred to as a composite number.
+
+def prime?(num)
+    if num < 0 or num == 0 or num == 1
+      return false
+    else
+      (2..num-1).to_a.all? do |possible_factor|
+        num % possible_factor != 0
+      end
+    end
+  end
